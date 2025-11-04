647d548378dac48c1f7c7b82aebf669adeeb4367
Spartak092225 <spartak092225@gmail.com>
2025-11-04 12:44:06 +0100

    Chang vite config and BrowserRouter to HasRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { Header } from './components/Header';
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import { CountriesProvider } from './CountriesContext';
-import RegionPage from './pages/RegionPage';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { CountriesProvider } from "./CountriesContext";
+import RegionPage from "./pages/RegionPage";
 
 function App() {
   return (
@@ -12,34 +12,16 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/Africa"
-            element={<RegionPage region="Africa" />}
-          />
-          <Route
-            path="/Asia"
-            element={<RegionPage region="Asia" />}
-          />
-          <Route
-            path="/Europe"
-            element={<RegionPage region="Europe" />}
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/Africa" element={<RegionPage region="Africa" />} />
+          <Route path="/Asia" element={<RegionPage region="Asia" />} />
+          <Route path="/Europe" element={<RegionPage region="Europe" />} />
           <Route
             path="/TheAmericas"
             element={<RegionPage region="Americas" />}
           />
-          <Route
-            path="/Oceania"
-            element={<RegionPage region="Oceania" />}
-          />
-          <Route
-            path="/Favorites"
-            element={<Favorites />}
-          />
+          <Route path="/Oceania" element={<RegionPage region="Oceania" />} />
+          <Route path="/Favorites" element={<Favorites />} />
         </Routes>
       </Router>
     </CountriesProvider>
