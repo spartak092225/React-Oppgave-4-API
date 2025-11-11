a2d009dc4c4281d3240ab6d2c87d2ff8fef5ed97
Spartak092225 <spartak092225@gmail.com>
2025-11-11 11:20:09 +0100

    Fix the favorite button size in mobile mode inside the modal.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,13 @@
-import styles from './Modal.module.css';
+import styles from "./Modal.module.css";
 
 export default function Modal({ children, onClose }) {
   return (
-    <div
-      className={styles.modalBackdrop}
-      onClick={onClose}
-    >
+    <div className={styles.modalBackdrop} onClick={onClose}>
       <div
         className={styles.modalContainer}
         onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
       >
-        <button
-          className={styles.closeBtn}
-          onClick={onClose}
-        >
+        <button className={styles.closeBtn} onClick={onClose}>
           ✕
         </button>
         {children}
